0cca8f24eff4ae4df5da8a661ace5938ed409924
Azeez Patan <azeez.p019@gmail.com>
2026-03-17 15:58:39 +0530

    api first test

--- api/login_api.py
+++ api/login_api.py
@@ -20,4 +20,8 @@ def login(email, password):
         headers = headers
     )
     return response
-    
+"""
+here whai got is we are sending a post request where response = request.post (url,json=payloads,headers=headers)
+here we have defined the url = Base_url/login paylods are defined as {email,password} json format headers  defined as content-type :application/json so 
+these variables will get utilised in fetching the response and we are validatinfg by saying that token shoud be preent in
+data where data = context.response.json()"""
